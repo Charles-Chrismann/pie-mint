0e1aac21ae4a440f3c828b29297d55fdbb4e0e44
Charles-Chrismann <charles.chrismann@free.fr>
2025-06-21 17:28:17 +0200

    fix: sub-event tracks + opti

--- mint-server/src/modules/sub-events/sub-events.service.ts
+++ mint-server/src/modules/sub-events/sub-events.service.ts
@@ -20,28 +20,40 @@ export class SubEventsService {
     .from(tracks_table)
     .where(eq(tracks_table.id, subEventId)).limit(1))[0]
 
-    const points = await this.drizzle.client
-    .select()
-    .from(track_points_table)
-    .where(eq(track_points_table.track_id, track.id))
+    const [points, segments] = await Promise.all([
+      this.drizzle.client
+      .select(
+        {
+          id: track_points_table.id,
+          lat: track_points_table.lat,
+          lng: track_points_table.lng,
+          is_first_point: track_points_table.is_first_point,
+          is_last_point: track_points_table.is_last_point,
+          track_id: track_points_table.track_id,
+        }
+      )
+      .from(track_points_table)
+      .where(eq(track_points_table.track_id, track.id)),
+  
+      this.drizzle.client
+      .select(
+        {
+          start_position_id: track_segments_table.start_position_id,
+          end_position_id: track_segments_table.end_position_id,
+        }
+      )
+      .from(track_segments_table)
+      .where(eq(track_segments_table.track_id, track.id))
 
-    const segments = await this.drizzle.client
-    .select()
-    .from(track_segments_table)
-    .where(eq(track_segments_table.track_id, track.id))
+    ])
 
-    // return {
-    //   track,
-    //   points,
-    //   segments
-    // }
 
     const pointsToReturn: typeof points = []
     const firstPoint = points.find(p => p.is_first_point)!
-    // firstPoint[0] = true
+    
     pointsToReturn.push(firstPoint)
 
-    let nextPointId = segments.find(s => s.start_position_id === firstPoint!.id)!.id
+    let nextPointId = segments.find(s => s.start_position_id === firstPoint!.id)!.end_position_id
     let nextPoint = points.find(p => p.id === nextPointId)
 
     while(nextPoint) {
