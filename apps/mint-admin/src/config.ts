423093e1199d0b6765b5c55685458fef3fbf43ff
Charles-Chrismann <charles.chrismann@free.fr>
2025-11-19 00:46:44 +0100

    mint-admin build

--- apps/mint-admin/src/config.ts
+++ apps/mint-admin/src/config.ts
@@ -1,9 +1,9 @@
-export const API_BASE_URL =
+const API_BASE_URL =
   ((typeof window !== 'undefined' && (window as any)._env_?.VITE_API_BASE_URL) ??
    import.meta.env.VITE_API_BASE_URL ??
    'http://localhost:3000');
 
-export const WS_URL =
+const WS_URL =
   ((typeof window !== 'undefined' && (window as any)._env_?.VITE_WS_URL) ??
    import.meta.env.VITE_WS_URL ??
    'http://localhost:3001');
